bfa7b37d6489de7370d357907aaab817e9986e88
Richard Clayton <richard.clayton@shopify.com>
2019-11-14 11:01:18 -0500

    add some exception handling

--- synthorg/presentation/command_line.rb
+++ synthorg/presentation/command_line.rb
@@ -14,23 +14,32 @@ class CommandLine
     @collectables.select do |item|
       item.is_a? type.nil? ? Object : Object.const_get(type)
     end
+  rescue NameError
+    puts "'#{type}' not a valid collectable type."
+    []
   end
 
   def bulk_value_update_by_type(percentage, type)
     find_all_of_type(type).map! do |item|
       item.value * percentage
     end
+  rescue NoMethodError
+    puts 'No collectables found.'
   end
 
   def get_collection_value(type = '')
-    find_all_of_type(type).reduce(0) do |sum, item|
+    "$#{find_all_of_type(type).reduce(0) do |sum, item|
       sum + item.value
-    end
+    end}"
+  rescue NoMethodError
+    puts 'No collectables found.'
   end
 
   def sell_on_kijiji_or_ebay(cutoff)
     @collectables.partition do |item|
       item.value < cutoff
     end
+  rescue NoMethodError
+    puts 'No collectables found.'
   end
 end
